1f452b2537ae5692fddb34dfdaed81ad32263738
Jakub Sielewicz <76447210+sielewa@users.noreply.github.com>
2022-02-15 13:51:15 +0100

    add database

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    console.log(err.name);
+    console.log(err.code);
+
+    res.status(500).json({
+        message: "Something went really wrong",
+    });
+})
 
 app.get('/', (req, res) => {
     res.send('hello world');
